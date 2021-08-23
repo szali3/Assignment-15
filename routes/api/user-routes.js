ec5741431df8409ed10108cff8363ea1ecb5b8e4
Zohaib Ali <62861582+szali3@users.noreply.github.com>
2021-08-23 09:06:39 -0400

    Update user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,17 +1,3 @@
-// example data
-// {
-//   "username": "lernantino",
-//   "email": "lernantino@gmail.com"
-// }
-
-// BONUS: Remove a user's associated thoughts when deleted.
-
-// /api/users/:userId/friends/:friendId
-
-// POST to add a new friend to a user's friend list
-
-// DELETE to remove a friend from a user's friend list
-
 const router = require('express').Router();
 
 const {
@@ -24,19 +10,20 @@ const {
   deleteFriend
 } = require('../../controllers/user-controller');
 
-// /api/pizzas
+// /api/users
 router
   .route('/')
   .get(getAllUsers)
   .post(createUser);
 
-// /api/pizzas/:id
+// /api/users/:id
 router
   .route('/:id')
   .get(getUserById)
   .put(updateUser)
   .delete(deleteUser);
 
+// /api/users/:userId/friends/:friendId
 router
   .route('/:userId/friends/:friendId')
   .post(addFriend)
