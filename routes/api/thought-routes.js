2c67f2295928daf57259117a96ae08c5bcdc4069
Zohaib Ali <62861582+szali3@users.noreply.github.com>
2021-08-23 09:04:49 -0400

    Update thought-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,29 +1,4 @@
-// /api/thoughts
-
-// GET to get all thoughts
-
-// GET to get a single thought by its _id
-
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-
-// // example data
-// {
-//   "thoughtText": "Here's a cool thought...",
-//   "username": "lernantino",
-//   "userId": "5edff358a0fcb779aa7b118b"
-// }
-// PUT to update a thought by its _id
-
-// DELETE to remove a thought by its _id
-
-// /api/thoughts/:thoughtId/reactions
-
-// POST to create a reaction stored in a single thought's reactions array field
-
-
-// DELETE to pull and remove a reaction by the reaction's reactionId value
 const router = require('express').Router();
-
 const {
   getAllThoughts,
   getThoughtById,
@@ -47,9 +22,14 @@ router
   .put(updateThought)
   .delete(removeThought);
 
-// /api/thoughts/:thoughtId/reactions>
+// /api/thoughts/:thoughtId/reactions
 router
   .route('/:thoughtId/reactions')
   .post(addReaction)
+
+// /api/thoughts/:thoughtId/reactions/:reactionID
+router
+  .route('/:thoughtId/reactions/reactionId')
   .delete(deleteReaction);
+
 module.exports = router;
